8890436131694619949c8a3c34e00cc94b073b61
Paul Frandsen <frandsen@eden.rutgers.edu>
2013-05-25 16:16:50 -0400

    Added comments and clarified some variable names

--- KMeans_SciPy.py
+++ KMeans_SciPy.py
@@ -10,48 +10,73 @@ import numpy as np
 from scipy import cluster
 import pylab as pl
 
-
+#assign file specified in argument to variable
 inFile = open(sys.argv[1])
+#assign number of k's specified in command to variable
 number_of_ks = int(sys.argv[2])
 
-def kmeans(inFile, number_of_ks):
+def kmeans(inFile, number_of_ks, num_iters):
+    '''
+    returns an array of character classes in order of character number
+    and an array of centroids, requires input of a file of rates separated
+    by newline characters, the number of desired k's, and the number of
+    iterations of the kmeans algorithm
+    '''
+    #make list of rates from rate file
     list_of_rates = [line for line in inFile.readlines()]
-    newest_list_of_rates = []
+    #create empty list to append rates without newline characters
+    rate_list = []
+    #loop through list of rates and remove newline characters, append
+    #rate represented as a float into empty rate list
     for i in list_of_rates:
         newp = float(i.strip("\n"))
-        newest_list_of_rates.append(newp)
+        rate_list.append(newp)
 
-    array = np.array(newest_list_of_rates, ndmin = 1)
+    #create a one dimensional array for input into SciPy's implementation of kmeans
+    array = np.array(rate_list, ndmin = 1)
 
+    #performs kmeans clustering on array
     KMeans_out = cluster.vq.kmeans2(array, k = number_of_ks, \
-        iter = 100, minit = 'points')
+        iter = num_iters, minit = 'points')
 
+    #assign the rates to variable as array
     all_rates = KMeans_out[1]
-
+    #assign centroids to variable as array
     centroids = KMeans_out[0]
-
+    #make a list of the number of k's specified by user
     list_ks = range(number_of_ks)
-    print list_ks
     count = 0
     number_in_rate_class = []
+    #loop through the number of user specified k's and determine how many characters
+    #belong to each rate class
     for i in list_ks:
-        numba = 0
+        total_char = 0
         for p in all_rates:
             if p == list_ks[count]:
-                numba += 1
-        number_in_rate_class.append(numba)
+                total_char += 1
+        number_in_rate_class.append(total_char)
         count += 1
 
+    #print the number of characters in each rate class followed by the centroid
+    #for that rate class, (coded to check to ensure that rate classes and centroids
+    #make sense in comparison to distribution of rates)
     for i in list_ks:
         print "Rate class " + str(i + 1) + " has " \
         + str(number_in_rate_class[i]) + \
         " observations, centroid is " + str(centroids[i]) + "!"
     return all_rates, centroids
 
-print kmeans(inFile, number_of_ks)
-#pl.hist(newest_list_of_rates, bins = 100, facecolor = 'red')
-#pl.title("Individual site CI's from insect transcriptome alignment")
-#pl.xlabel("rates")
-#pl.ylabel("Frequency")
-#pl.show()
+print kmeans(inFile, number_of_ks, 100)
+
+def histogram_generator(rate_list, num_bins, title):
+    '''
+    Plots histogram given a user defined rate list, user
+    specified number of bins, and user specified title
+    '''
+    pl.hist(rate_list, bins = num_bins, facecolor = 'red')
+    pl.title(title)
+    pl.xlabel("rates")
+    pl.ylabel("Frequency")
+    pl.show()
+
 inFile.close()
--- KMeans_scikit_learn.py
+++ KMeans_scikit_learn.py
@@ -1,69 +1,110 @@
-import sys
+#import sys module to take command line arguments
+import sys  
+#import time to keep track of time analysis takes
 import time
 
-import numpy as np
+import numpy as np 
 import pylab as pl
 
 from sklearn import metrics
 from sklearn.cluster import KMeans
-from sklearn.preprocessing import scale
+from sklearn.preprocessing import scale 
 
 from collections import defaultdict
 
-start = time.clock()
+
+#open file with site rates, each rate must be separated by newline character "\n"
 file1 = sys.argv[1]
+#takes user input for number of desired clusters
 number_of_ks = int(sys.argv[2])
 
+#open file for reading and save in variable inFile to feed to function
 inFile = open(file1)
 
-
 def kmeans(inFile, number_of_ks):
-    list_of_rates = [line for line in inFile.readlines()]
-    newest_list_of_rates = []
-    for i in list_of_rates:
-        newp = float(i.strip("\n"))
-        newest_list_of_rates.append([newp])
+    '''
+    Takes, as input, a file with rates separated by newline characters, and
+    the number of desired clusters, then performs kmeans clustering
+    and returns a dictionary with character numbers as keys and
+    rates as values
+    '''
+    #start clock to keep track of time
+    start = time.clock()
+    #read rates into list
+    rate_list_lines = [line for line in inFile.readlines()]
+    #create new list for rates without newline characters
+    rate_list = []
+    #remove newline characters and add to new list of rates
+    for i in rate_list_lines:
+        float_rate = float(i.strip("\n"))
+        rate_list.append([float_rate])
 
-    array = np.array(newest_list_of_rates, ndmin = 1)
-
-    print array
-
-    #array = scale(array)
+    #create 1 dimensional array of rates for input into scikit_learn
+    #implementation of kmeans
+    array = np.array(rate_list, ndmin = 1)
 
     #print array
 
+    #scale numbers in array
+    #array = scale(array)
+
+    #print array
+    #create a KMeans object from scikit_learn
     KMeans_out = KMeans(init='k-means++', n_clusters = number_of_ks, \
         n_init = 10)
 
+    #perform KMeans clustering on data array from site rates see functions of 
+    #scikit learn KMeans class here: http://tinyurl.com/q3qjxly
     KMeans_out.fit(array)
 
+    #store centroids
     centroids = KMeans_out.cluster_centers_
-    list_of_categories = KMeans_out.labels_
-    char_number = 1
-    dictionary_of_characters_and_rates = {}
-    for i in list_of_categories:
-    	dictionary_of_characters_and_rates[char_number] = i
+    #store individual rates in a list, should be in character order
+    rate_categories = KMeans_out.labels_
+    #create empty dictionary to store rates and associated characters
+    dictionary_of_rates = {}
+    char_number = 1    
+    #loop through list of rate categories and add them to dictionary with associated character
+    for i in rate_categories:
+    	dictionary_of_rates[char_number] = i
     	char_number += 1
-
+    #print centroid info to terminal window
     print "Centroids: " + str(centroids)
-    print "All categories: " + str(list_of_categories)
+    #print list of categories to terminal
+    print "All categories: " + str(rate_categories)
 
+    #stop clock and output total time
     stop = time.clock()
     time_taken = "This analysis took " + str(stop - start) + "seconds!"
     print time_taken
 
-    return dictionary_of_characters_and_rates
+    #return the dictionary with the rates
+    return dictionary_of_rates
 
 def RAxML_categories(inFile, number_of_ks):
+    '''
+    Takes as input the file with rates separated by newline characters
+    and the desired number of rate categories, performs kmeans on the rates
+    and outputs them into a RAxML partition scheme file called "RAxML_scheme.txt"
+    '''
+    #call kmeans function and assign variable to the dictionary of rate classes
 	characters_and_rates = kmeans(inFile, number_of_ks)
+    #make new dicitonary to assign characters to values
 	output_dict = defaultdict(list)
+    #loops through dicitonary of characters and rate classes and assign characters
+    #to appropriate rate class in new dictionary
 	for k, v in characters_and_rates.iteritems():
 		output_dict[v].append(k)
-
+    
+    #creat new file to write RAxML partition scheme definition
 	outFile = open("RAxML_scheme.txt", "a")
+    #start counter for different partition numbers
 	partition = 0
+    #loops through dictionary of partitions and output information into
+    #RAxML scheme definition
 	for i in range(len(output_dict)):
-		outFile.write("Partition " + str(partition) + ", DNA = " + str(output_dict[partition]).strip("[]") + "\n")
+		outFile.write("Partition " + str(partition) + ", DNA = " + \
+            str(output_dict[partition]).strip("[]") + "\n")
 		print "Partition " + str(partition) + ", DNA = " + str(output_dict[partition])
 		partition += 1
 	outFile.close()
